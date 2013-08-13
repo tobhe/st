f80ab27ee075f00699df793e7d9a33c29b58a97a
Jari Aalto <jari.aalto@cante.net>
2013-08-13 22:55:07 +0300

    debian/repack.sh: update

--- debian/repack.sh
+++ debian/repack.sh
@@ -2,7 +2,7 @@
 #
 #   Copyright
 #
-#       Copyright (C) 2008-2009 Jari Aalto <jari.aalto@cante.net>
+#       Copyright (C) 2008-2010 Jari Aalto <jari.aalto@cante.net>
 #
 #   License
 #
@@ -22,35 +22,63 @@
 set -e
 set -u
 
-Init ()
+DIR=""
+
+Initialize ()
 {
     # Check depends
 
-    [ -x /bin/mktemp ] || Die "[ERROR]: mktemp (coreutils) not installed."
-    [ -x /bin/bzip2  ] || Die "[ERROR]: bzip2 (bzip2) not installed."
-    [ -x /bin/gzip   ] || Die "[ERROR]: gzip (gzip) not installed."
-    [ -x /bin/tar    ] || Die "[ERROR]: tar (tar) not installed."
+    [ -x /bin/mktemp ] || Die "[ERROR]: mktemp (pkg: coreutils) not installed."
+    [ -x /bin/bzip2  ] || Die "[ERROR]: bzip2 (pkg: bzip2) not installed."
+    [ -x /bin/gzip   ] || Die "[ERROR]: gzip (pkg: gzip) not installed."
+    [ -x /bin/tar    ] || Die "[ERROR]: tar (pkg: tar) not installed."
+}
+
+InitializeZip ()
+{
+    [ -x /usr/bin/unzip ] || Die "[ERROR]: unzip (pkg: unzip) not installed."
+}
+
+InitializeRar ()
+{
+    [ -x /usr/bin/unrar ] || Die "[ERROR]: unrar (pkg: unrar) not installed."
 }
 
 Help ()
 {
     echo "
 SYNOPSIS
-  repack.sh --upstream-source <ver> <downloaded file> [package]
+  repack.sh [--upstream-source] <VER> <downloaded file> [PACKAGE]
 
 DESCRIPTION
-    Repackage upstream source. The command line arguments are due
-    to uscan(1) order. The PACKAGE argument is optional.
+    Repackage upstream source. The command line arguments are
+    according to to uscan(1) order. The PACKAGE argument is optional.
+
+    Can also repack *.zip and *.rar files.
+
+OPTIONS
+    --upstream-source
+	Option is ignored. It is passed from uscan(1) when debian/watch
+	file is read.
 
 EXAMPLES
-    Repack foo-1.1.tar.gz as bar-1.10.tar.gz
+    To manually repack original upstream source foo-1.1.tar.gz to
+    bar-1.10.orig.tar.gz:
 
         repack.sh 1.10 foo-1.1.tar.gz bar
 
+    Save this file to debian/repack.sh and add following line to debian/watch:
+
+        version=3
+        http://example.com .*package-(.+).tar.gz debian debian/repack.sh
+
 AUTHOR
-    Jari Aalto <jari.aalto@cante.net>. Licenced under GPL v2 or,
-    at your option, any later version.
-"
+    Copyright (C) 2008-2010 Jari Aalto <jari.aalto@cante.net>
+
+    Released under the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version."
+
     exit 0
 }
 
@@ -78,14 +106,14 @@ Die ()
 AtExit ()
 {
     if [ "$DIR" ]; then
-	[ -d "$DIR" ] && Run rm -rf "$DIR"
+	[ -d "$DIR" ] && rm -rf "$DIR"
     fi
 }
 
 DebianVersion ()
 {
-    # YYYYMMDD => YYYY.MMDD
-    echo $1 | sed 's/^\([0-9][0-9][0-9][0-9]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1.\2\3/'
+    # No version conversions yet
+    echo $1
 }
 
 DebianTar ()
@@ -95,11 +123,15 @@ DebianTar ()
     local file=$3
     local pkg=$4
 
+    # Convert suffixes
+
+    file=$(echo $file | sed -e 's,.orig,,' -e 's,\(tgz\|zip\|rar\)$,tar.gz,' )
+
     # If version is same, use original file
 
     if [ "$ver" = "$dver" ]; then
 	if [ "$pkg" ]; then
-	    echo $file | sed "s,.*$ver,$pkg-$ver,"
+	    echo $file | sed "s,.*$ver,${pkg}_$ver.orig,"
 	else
             echo $file
 	fi
@@ -107,10 +139,10 @@ DebianTar ()
     fi
 
     if [ "$pkg" ]; then
-	echo $file | sed -e "s,.*$ver,$pkg-$dver,"
+	echo $file | sed -e "s,.*$ver,${pkg}_$dver.orig,"
     else
         # replace with new version
-	echo $file | sed -e "s,$ver,$dver,"
+	echo $file | sed -e "s,$ver,$dver.orig,"
     fi
 }
 
@@ -123,7 +155,7 @@ Pkg ()
     else
 
 	# package-1.1.tar.gz => package
-	echo $file | sed "s,-[0-9].*,,"
+	echo $file | sed "s,[-_][0-9].*,,"
     fi
 }
 
@@ -137,32 +169,84 @@ Version ()
     fi
 
     echo $file |
-    sed -e "s,\.tar.*,," \
+    sed -e "s,\.orig.*,," \
+	-e "s,\.tar.*,," \
+	-e "s,\.tgz,," \
+	-e "s,\.tbz,," \
+	-e "s,\.tbz2,," \
+	-e "s,\.zip,," \
+	-e "s,\.7z,," \
+	-e "s,\.rar,," \
+	-e "s,\.lzma,," \
+	-e "s,\.xz,," \
         -e "s,$pkg[-_],,"
+}
+
+Cleanup ()
+{
+    [ "$1" ] || return 1
+
+    local dir
+    dir=$1
+
+    find "$dir" \
+	\( \
+	-iname "*.exe" \
+	-o -name "*.swp" \
+	-o -name "DEADJOE" \
+	-o -name "*.[~#]" \
+	-o -name ".gitignore" \
+	-o -name ".bzrignore" \
+	-o -name ".svnignore" \
+	-o -name ".cvsignore" \
+	-o -path "*/debian/*" \
+	-o -path "*/debian" \
+	-o -path "*/CVS*" \
+	-o -path "*/.svn*" \
+	-o -path "*/.darcs*" \
+	-o -path "*/.bzr*" \
+	-o -path "*/.hg*" \
+	-o -path "*/.git*" \
+	\) \
+	-print0 |
+    xargs --null --no-run-if-empty rm -rf --verbose
+
+    #	Remove executables
+
+    find "$dir" -type f -print0 |
+        xargs --null --no-run-if-empty file |
+    awk -F: '$2 ~ /ELF|COFF|LSB exe/ {print $1}' |
+        xargs --no-run-if-empty rm --verbose
 }
 
 Main ()
 {
-    Init
+    if [ $# -eq 0 ]; then
+        Help
+    fi
 
-    DIR=
-
-    #  Ignore uscan(1) argument --upstream-version in $1
+    Initialize
 
     case "$1" in
 	--help|-h)
-		Help
-		;;
-	--*)	shift
-		;;
+	    Help
+	    ;;
+	--*)
+	    shift
+	    #  Ignore uscan(1) argument --upstream-version in $1
+	    ;;
     esac
 
     VER="$1"
-    FILE="$2"
+    FILENAME="$2"
+    DIR=
 
-    if [ ! -f "$FILE" ]; then
-	Die "[ERROR] Arg 2. File does not exist: $FILE"
+    if [ ! -f "$FILENAME" ]; then
+	Die "[ERROR] Arg 2. File does not exist: $FILENAME"
     fi
+
+    FILE_DIR=$(dirname $FILENAME)
+    FILE=$(basename $FILENAME)
 
     PKG=${3:-$(Pkg $FILE)}
 
@@ -183,9 +267,13 @@ Main ()
 
     REPACK_DIR="$PKG-$DVER.orig"
 
-    DIR=$(Run mktemp -d ./tmp.repack.XXXXXX)
+    DIR=$(mktemp -d ./tmp.repack.XXXXXX)
 
-    echo "Repacking $FILE as $PKG-$DVER"
+    if [ ! "$DIR" ]; then
+	Die "[INTERNAL ERROR] mktemp(1) failed. Debug with 'sh -x PROGRAM'"
+    fi
+
+    echo "Repacking $FILENAME as $PKG-$DVER"
 
     #	Create an extra directory to cope with tarballs that
     #	do not have root/ directory
@@ -193,24 +281,63 @@ Main ()
     UP_BASE="$DIR/unpack"
     Run mkdir "$UP_BASE"
 
-    Run tar -C "$UP_BASE" -xf "$FILE"
+    curdir=$(pwd)
+
+    case "$FILENAME" in
+	*.gz | *.bz2 )
+
+	    Run tar -C "$UP_BASE" -xf "$FILENAME"
+	    ;;
+
+	*.zip)
+	    InitializeZip
+
+	    adir=$(dirname $FILENAME)
+	    name=$(basename $FILENAME)
+
+	    Run cd "$UP_BASE"
+	    Run unzip "$curdir/$adir/$name" || return 1
+
+	    cd $curdir
+	    ;;
+
+	*.rar)
+	    InitializeRar
+
+	    adir=$(dirname $FILENAME)
+	    name=$(basename $FILENAME)
+
+	    Run cd "$UP_BASE"
+	    Run unrar x "$curdir/$adir/$name" || return 1
+
+	    cd $curdir
+
+	    ;;
+
+	*)  Die "Unknonw file format: $FILENAME"
+	    ;;
+    esac
 
     if [ $(ls -1 "$UP_BASE" | wc -l) -eq 1 ]; then
 	# Tarball does contain a root directory
 	UP_BASE="$UP_BASE/$(ls -1 "$UP_BASE")"
     fi
 
-    #	Remove files if needed
+    if [ ! "$UP_BASE" ]; then
+	Die "[INTERNAL ERROR] UP_BASE not set"
+    fi
+
+    Cleanup "$UP_BASE"
 
     #	Repack
 
     Run mv "$UP_BASE" "$DIR/$REPACK_DIR"
 
-    #	Don't use pipes. Errors are not handled correctly if pipes aree used.
+    #	Don't use pipes. Errors are not handled well with them.
 
     Run tar -C "$DIR" -cf "$DIR/repacked.tar" "$REPACK_DIR"
 
-    #   The .orig file must uxe gzip compression
+    #   The .orig file must use gzip compression
 
     tar="$DIR/repacked.tar"
 
@@ -220,6 +347,12 @@ Main ()
 	    ;;
 	*.gz)
 	    ;;
+	*.zip)
+	    DFILE=$(echo $DFILE | sed "s/.zip/.gz/")
+	    ;;
+	*.rar)
+	    DFILE=$(echo $DFILE | sed "s/.rar/.gz/")
+	    ;;
 	 *)
 	    Die "Unknown *.suffix in $DFILE"
 	    ;;
@@ -228,17 +361,17 @@ Main ()
     suffix=".gz"
 
     Run gzip --best "$tar"
+
+    if [ -f "$DFILE" ]; then
+	echo "Warning, overwriting $DFILE"
+    fi
+
     Run mv "$tar$suffix" "$DFILE"
 
     echo "Done $DFILE"
 }
 
 trap AtExit QUIT INT EXIT
-
-if [ $# -eq 0 ]; then
-    Help
-else
-    Main "$@"
-fi
+Main "$@"
 
 # End of file
