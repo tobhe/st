bc3c1a989dc19e732544ad2cfca25005a11d31f9
Jari Aalto <jari.aalto@cante.net>
2013-08-13 22:53:33 +0300

    debian/debian-vars.mk: update

--- debian/debian-vars.mk
+++ debian/debian-vars.mk
@@ -1,26 +1,31 @@
-# debian-vars.mk -- Common variables
+#!/usr/bin/make -f
+#
+#   debian-vars.mk -- Common variables
 #
 #   Copyright
 #
-#	Copyright (C) 2005-2009 Jari Aalto <jari.aalto@cante.net>
+#	Copyright (C) 2005-2010 Jari Aalto <jari.aalto@cante.net>
 #
 #   License
 #
-#	This program is free software; you can redistribute it and or
-#	modify it under the terms of the GNU General Public License as
-#	published by the Free Software Foundation; either version 2 of
-#	the License, or (at your option) any later version.
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful, but
-#	WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#	General Public License for more details at
-#	<http://www.gnu.org/copyleft/gpl.html>.
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #   Description
 #
-#	This is GNU makefile part, that defines common variables and
-#	macros to be used from debian/rules. To install, add this:
+#	This is GNU makefile part that defines common variables and
+#	macros to be used from debian/rules. To install, add following
+#	to the beginning of debian/rules:
 #
 #	    PACKAGE = foo
 #	    include debian/debian-vars.mk
@@ -31,24 +36,29 @@ endif
 
 PACKAGE		?= foo
 PIXPACKAGE	?= $(PACKAGE)
+ICONPACKAGE	?= $(PACKAGE)
 TOPDIR		:= $(shell pwd)
-PKGDIR		= $(CURDIR)/debian/$(PACKAGE)
+PKGDIR		= $(shell pwd)/debian/$(PACKAGE)
 
 SHAREROOTDIR	= $(PKGDIR)/usr/share
 SHAREDIR	= $(SHAREROOTDIR)/$(PACKAGE)
 DOCROOTDIR	= $(SHAREROOTDIR)/doc
-PKGDOCDIR	= $(SHAREROOTDIR)/doc/$(PACKAGE)
+PKGDOCDIR	= $(DOCROOTDIR)/$(PACKAGE)
 SITELISPDIR	= $(SHAREROOTDIR)/emacs/site-lisp
 PKGLISPDIR	= $(SITELISPDIR)/$(PACKAGE)
 DESKTOPDIR	= $(SHAREROOTDIR)/applications
+XSESSIONDIR	= $(SHAREROOTDIR)/xsessions
 LOCALEDIR	= $(SHAREROOTDIR)/locale
 INFODIR		= $(SHAREROOTDIR)/info
 PIXDIR		= $(SHAREROOTDIR)/pixmaps
-PKGPIXDIR	= $(SHAREROOTDIR)/pixmaps/$(PIXPACKAGE)
+PKGPIXDIR	= $(PIXDIR)/$(PIXPACKAGE)
+ICONDIR		= $(SHAREROOTDIR)/icons
+PKGICONDIR	= $(ICONDIR)/$(ICONPACKAGE)
 
 MANROOTDIR	= $(SHAREROOTDIR)/man
 MAN1DIR		= $(MANROOTDIR)/man1
 MAN5DIR		= $(MANROOTDIR)/man5
+MAN7DIR		= $(MANROOTDIR)/man7
 MAN8DIR		= $(MANROOTDIR)/man8
 
 LIBROOTDIR	= $(PKGDIR)/usr/lib
@@ -56,50 +66,21 @@ LIBDIR		= $(LIBROOTDIR)/$(PACKAGE)
 LIBPERLDIR	= $(LIBROOTDIR)/perl
 
 BINDIR		= $(PKGDIR)/usr/bin
+ETCDIR		= $(PKGDIR)/etc
+PKGETCDIR       = $(PKGDIR)/etc/$(PACKAGE)
 SBINDIR		= $(PKGDIR)/usr/sbin
+GAMEBINDIR	= $(PKGDIR)/usr/games
 
 INSTALL		?= /usr/bin/install
+INSTALL_DIR	= $(INSTALL) -m 755 -d
 INSTALL_DATA	= $(INSTALL) -p -m 644
+
+# The difference is that "BIN" may contain binary strip option
 INSTALL_SCRIPT	= $(INSTALL) -p -m 755
 INSTALL_BIN	= $(INSTALL) -p -m 755
-INSTALL_DIR	= $(INSTALL) -m 755 -d
-
-#######################################################################
-# These are used for cross-compiling and for saving the configure script
-# from having to guess our platform (since we know it already)
-#
-# [Add this]
-# config.status: configure
-#	...
-#	./configure --host=$(DEB_HOST_GNU_TYPE) \
-#		    --build=$(DEB_BUILD_GNU_TYPE) \
-
-DEB_HOST_GNU_TYPE   ?= $(shell dpkg-architecture -qDEB_HOST_GNU_TYPE)
-DEB_BUILD_GNU_TYPE  ?= $(shell dpkg-architecture -qDEB_BUILD_GNU_TYPE)
-
-#######################################################################
-# [Add this to use multiple CPU cores]
-#	build-stamp:
-#		$(MAKE) $(MAKE_FLAGS)
 
 ifeq (,$(findstring nostrip,$(DEB_BUILD_OPTIONS)))
     INSTALL_BIN += -s
-endif
-
-CFLAGS		= -Wall -g
-
-ifneq (,$(findstring noopt,$(DEB_BUILD_OPTIONS)))
-    CFLAGS += -O0
-else
-    CFLAGS += -O2
-endif
-
-MAKE_FLAGS	=
-CPU_COUNT	:= $(shell fgrep -c processor /proc/cpuinfo 2> /dev/null | \
-		     egrep "^[2-9]$$|^[0-9][0-9]$$")
-
-ifneq ($(CPU_COUNT),)
-    MAKE_FLAGS	+= -j$(CPU_COUNT)
 endif
 
 # End of Makefile part
